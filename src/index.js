c539a9afe34f03b6be97e4c6be0fc36149343401
GemmaClaveroDelMoral <casagemmayraul@yahoo.es>
2024-05-06 09:08:49 -0500

    comentarios de version

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+//Para version actual
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+// Practica si bajaramos de version a 17.0
+// Revisar video clase React intro - 21.Versiones de React 
